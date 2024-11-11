e38359acf1c717f7fadfa4881acedbaf16272079
OrangeWeb <8dev.il3@gmail.com>
2024-11-11 23:14:15 +0100

    corretti pulsanti

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,27 +1,9 @@
 <template>
-    <button type="button" class="px-3 text-sm py-1.5 rounded-md text-white truncate w-full transition-colors" :class="colorClasses">
+    <button type="button" class="px-3 text-sm py-1.5 rounded-md text-white truncate w-full transition-colors">
         <slot />
     </button>
 </template>
 
 <script setup>
-import { computed } from 'vue';
-
-const props = defineProps({
-    color: String
-});
-
-const colorClasses = computed(()=>{
-    switch (props.color) {
-        case 'blue':
-        return 'bg-blue-600 hover:bg-blue-500';
-
-        case 'violet':
-        return 'bg-violet-600 hover:bg-violet-500';
-
-        case 'fuchsia':
-        return 'bg-fuchsia-600 hover:bg-fuchsia-500';
-    }
-});
 
 </script>
